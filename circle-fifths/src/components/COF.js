0d72e9d37b1c3e2e1be670f5580bc70ba1c45679
rivera-davidkyle <rivera.davidkyle@gmail.com>
2022-04-19 16:47:32 -0400

    Added components

---
+++ circle-fifths/src/components/COF.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './COF.css';
+
+const COF = () => {
+  return (
+    <div>
+        <h2>Circle</h2>
+    <div class="circle">
+        
+    </div>
+    </div>
+  )
+}
+
+export default COF
